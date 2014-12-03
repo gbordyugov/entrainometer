5c643853b649c4dfb6701765926c06b91d577e8c
Grigory Bordyugov <gregbg@gmail.com>
2014-12-03 13:04:09 +0100

    refactored bluebars.py a bit

--- bluebars.py
+++ bluebars.py
@@ -3,25 +3,24 @@
 from sys import argv
 from numpy import zeros_like, abs, arange, loadtxt
 
-from pylab import figure, plot, fill_between, xlim, ylim, xlabel, ylabel,\
-                  savefig
+from pylab import figure 
 
 
-def plot_scn(scn, time, data, temp):
-  figure()
+def plot_scn(time, data, temp):
+  fig = figure()
+  ax = fig.add_subplot(111)
+
   time = time/24.0
-  plot(time, data, '-')
+  ax.plot(time, data, '-')
 
-  temp = temp-temp.min()
-  temp = temp/temp.max()*2.0
-  fill_between(time, 2.0, temp, color='blue', alpha=0.3)
+  normalized_temp = (temp-temp.min())/(temp.max()-temp.min())*2.0
+  ax.fill_between(time, 2.0, normalized_temp, color='blue', alpha=0.3)
 
-  pdfname = output_dir + '/SCN%02d.pdf'%scn
-  print 'plotting SCN %d into %s'%(scn, pdfname)
-  xlabel('time [h]')
+  ax.set_xlabel('time [h]')
+  ax.set_ylabel('Bioluminescence, temperature')
+  ax.set_xlim(time.min()-1.0, time.max()+1.0)
 
-  xlim(time.min()-1.0, time.max()+1.0)
-  savefig(pdfname)
+  return fig
 
 
 def segmenter(time, data, temp):
@@ -43,6 +42,37 @@ def segmenter(time, data, temp):
          for (a,b) in zip(i[:-1], i[1:])]
 
 
+def main(filename, output_dir):
+  u = loadtxt(filename, unpack=True)
+  scn_time = u[0]
+  
+  ncols = u.shape[0]
+  nSCN  = (ncols-1)/2
+  
+  for scn in xrange(nSCN):
+    data_ix = 1 + scn*2
+    temp_ix = data_ix + 1
+  
+    scn_data = u[data_ix]
+    scn_temp = u[temp_ix]
+  
+    dfit = {}; tfit = {}
+  
+    segs = segmenter(scn_time, scn_data, scn_temp)
+    seg = segs[1]
+    time, data, temp = seg[0], seg[1], seg[2]
+  
+    # there can be some trailing zeros in the data
+    while data[-1] == 0.0:
+      time = time[:-1]
+      data = data[:-1]
+      temp = temp[:-1]
+      
+    fig = plot_scn(time, data, temp)
+    fname = output_dir + '/SCN%02d.pdf'%scn
+    print 'saving to %s'%fname
+    fig.savefig(fname)
+
 
 # main entry point
 
@@ -53,31 +83,5 @@ if len(argv) < 3:
 filename   = argv[1]
 output_dir = argv[2]
 
-u = loadtxt(filename, unpack=True)
-scn_time = u[0]
-
-ncols = u.shape[0]
-nSCN  = (ncols-1)/2
-
-for scn in xrange(nSCN):
-# for scn in xrange(1):
-  data_ix = 1 + scn*2
-  temp_ix = data_ix + 1
-
-  scn_data = u[data_ix]
-  scn_temp = u[temp_ix]
-
-  dfit = {}; tfit = {}
-
-  segs = segmenter(scn_time, scn_data, scn_temp)
-  seg = segs[1]
-  time, data, temp = seg[0], seg[1], seg[2]
-
-  # there can be some trailing zeros in the data
-  while data[-1] == 0.0:
-    time = time[:-1]
-    data = data[:-1]
-    temp = temp[:-1]
-    
-  plot_scn(scn, time, data, temp)
+main(filename, output_dir)
 
